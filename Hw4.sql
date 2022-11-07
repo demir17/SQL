94dcc31ff48a3f2dfb6574b84911fbaee5d9e820
demir17 <42421998+demir17@users.noreply.github.com>
2022-11-07 23:19:51 +0300

    Create Hw4.sql

---
+++ Hw4.sql
@@ -0,0 +1,12 @@
+SELECT DISTINCT replacement_cost FROM film
+
+SELECT COUNT(*) FROM film
+
+SELECT COUNT(*) FROM film
+WHERE title LIKE 'T' and rating= 'G'
+
+SELECT COUNT (country) FROM country
+WHERE country LIKE '_____';
+
+SELECT COUNT (city) FROM city
+WHERE city LIKE '%R' OR city LIKE '%r';
